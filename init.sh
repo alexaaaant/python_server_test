8fbed90f3e735175770bf4e26684b5e710610bb5
alexaaaant <alexaaaant@gmail.com>
2020-04-12 12:54:57 +0300

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo gunicorn --bind='0.0.0.0:8080' hello:app
 sudo /etc/init.d/nginx restart
+sudo gunicorn --bind='0.0.0.0:8080' hello:app
