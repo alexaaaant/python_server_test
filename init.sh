3518101dc20ca1a05247e6081697062b3254b39f
alexaaaant <alexaaaant@gmail.com>
2020-04-12 13:14:39 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:app
+sudo gunicorn -b 0.0.0.0:8080 -c hello.py hello:app
