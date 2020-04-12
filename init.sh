1365593e245766e2cc69059c34112a033a661cca
alexaaaant <alexaaaant@gmail.com>
2020-04-12 12:32:57 +0300

    fix init

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py  /etc/gunicorn.d/test
 sudo gunicorn -c /etc/gunicorn.d/test /home/box/web/hello.py 
 sudo /etc/init.d/nginx restart
