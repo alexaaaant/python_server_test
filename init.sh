c68c6a399306c139bbf27102eb8039e943e69952
alexaaaant <alexaaaant@gmail.com>
2020-04-12 17:07:00 +0300

    Добавлен запуск django через gunicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn --bind='0.0.0.0:8080' hello:application
+sudo gunicorn --bind='0.0.0.0:8080' hello:application --daemon
+sudo gunicorn --bind='0.0.0.0:8000' ask.wsgi
