4da6715ec2c400f230b62cf67a381eb23af64290
alexaaaant <alexaaaant@gmail.com>
2020-04-12 13:29:49 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import re
 
 
-def app(env, start_response):
+def application(env, start_response):
     query_string = env["QUERY_STRING"]
     ans = '\n'.join(re.findall(r'([^?=&]+=[^&]*)+', query_string))
     start_response("200 OK", [("Content-Type", "text/plain")])
