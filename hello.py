16d4e421433cd790f2eecd40f879f23ecfa64921
alexaaaant <alexaaaant@gmail.com>
2020-04-12 14:31:34 +0300

    добавление кодировки

--- hello.py
+++ hello.py
@@ -5,5 +5,4 @@ def application(env, start_response):
     query_string = env["QUERY_STRING"]
     ans = '\n'.join(re.findall(r'([^?=&]+=[^&]*)+', query_string))
     start_response("200 OK", [("Content-Type", "text/plain")])
-    return [bytes(ans)]
-
+    return [bytes(ans, encoding='utf8')]
