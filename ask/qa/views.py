ad525634d61a31c7cdc98458eb132c36ffb99ff4
alexaaaant <alexaaaant@gmail.com>
2020-04-12 16:58:28 +0300

    Удалены лишние функции

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK 200')
-
-def login(request, *args, **kwargs):
-    return HttpResponse('OK login page')
-
-def question(request, *args, **kwargs):
-    return HttpResponse('OK question page')
+    return HttpResponse('OK')
