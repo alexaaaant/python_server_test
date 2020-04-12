a947675e9d653cce702482c5e52f2fada3fa7003
alexaaaant <alexaaaant@gmail.com>
2020-04-12 13:11:55 +0300

    update

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ def app(env, start_response):
     query_string = env["QUERY_STRING"]
     ans = '\n'.join(re.findall(r'([^?=&]+=[^&]*)+', query_string))
     start_response("200 OK", [("Content-Type", "text/plain")])
-    return [ans]
+    return [bytes(ans)]
 
