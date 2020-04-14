37f4902c765e599a68b32d9a688cc05004d453af
alexaaaant <alexaaaant@gmail.com>
2020-04-14 21:35:23 +0300

    обновлен init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn --bind='0.0.0.0:8080' hello:application --daemon
-sudo gunicorn --bind='0.0.0.0:8000' ask.wsgi
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
