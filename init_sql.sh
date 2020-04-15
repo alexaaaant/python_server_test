c9fcba569d36fa30f4cd3e8efef63429357ce8cd
alexaaaant <alexaaaant@gmail.com>
2020-04-15 22:25:45 +0300

    обновлен конфиг sql

--- init_sql.sh
+++ init_sql.sh
@@ -1,3 +1,11 @@
+sudo apt update
+sudo apt install python3.5
+sudo apt install python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE USER 'sasha'@'localhost'"
 mysql -uroot -e "SET PASSWORD FOR 'sasha'@'localhost' = PASSWORD('sasha')"
